fe3e15fd229d404e01fcf81690b838c7ddc54687
Cameron Pavao <cam9548@gmail.com>
2018-10-21 22:51:58 -0500

    updated pages
    things work kinda

--- src/App.js
+++ src/App.js
@@ -1,84 +1,72 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar, Nav, NavItem, Glyphicon } from 'react-bootstrap';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import Home from './components/home'
 import About from './components/about'
 import Resume from './components/resume'
 import Contact from './components/contact'
 import Projects from './components/projects'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+        isOpen: false
+    };
+}
+toggle() {
+    this.setState({
+        isOpen: !this.state.isOpen
+    });
+}
   render() {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <h1>
-              Hi! I'm Cameron Pavao
-            </h1>
-            <small>
-              Welcome to my site
-            </small>
-          </header>
-          <Navbar bsStyle="nav-custom">
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">
-                <Glyphicon glyph="home" />
-              </Link>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              <Link to="/about">
+          <Navbar color="inverse" light expand="md">
+            <NavbarBrand >
+            <NavLink tag={Link} to="/">
+                Cam
+              </NavLink>
+            </NavbarBrand>
+          <Nav className="ml-auto" navbar>           
+            <NavItem >
+              <NavLink tag={Link} to="/about">
                 About
-              </Link>
+              </NavLink>
             </NavItem>
-            <NavItem eventKey={2} href="#">
-              <Link to="/resume">
+            <NavItem >
+              <NavLink tag={Link} to="/resume">
                 Resume
-              </Link>
+              </NavLink> 
             </NavItem>
-            <NavItem eventKey={3} href="#">
-              <Link to="/projects">
+            <NavItem >
+              <NavLink tag={Link} to="/projects">
                 Projects
-              </Link>
+              </NavLink>
             </NavItem>
-            <NavItem eventKey={4} href="#">
-              <Link to="/contact">
+            <NavItem >
+              <NavLink tag={Link} to="/contact">
                 Contact
-              </Link>
+              </NavLink>
             </NavItem>
           </Nav>
           </Navbar>
-          <body>
-            <h2>
-              Socials
-            </h2>
-            <div >
-              <a href="https://www.linkedin.com/in/cameron-pavao-0928b2140/">
-                <img width={50} height={50} alt="25" src="/linkedin.ico" className='socials' />
-              </a>
-              <a href="https://www.twitter.com/allcamadianeh">
-                <img width={50} height={50} alt="25" src="/twitter.ico" className='socials' />
-              </a>
-              <a href="mailto:cameronpavao@gmail.com">
-                <img width={50} height={50} alt="25" src="/mail.ico" className='socials' href=""/>
-              </a>
-              <a href="https://www.instagram.com/camcorderpavao">
-                <img width={50} height={50} alt="25" src="/instagram.ico" className='socials' />
-              </a>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/resume" component={Resume} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
             </div>
-          </body>
-
-
-          <Route exact path="/" component={App} />
-          <Route path="/about" component={About} />
-          <Route path="/resume" component={Resume} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
-        </div>
       </Router>
     );
   }
