922df02df5d91c51b28c8fb637ad5e6fa78eee4f
Cameron Pavao <cam9548@gmail.com>
2018-10-28 12:27:51 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59b0903c57c081fa89f951be3a080456.js"
+  "/precache-manifest.563619330d9547b690133a98a61248b6.js"
 );
 
 workbox.clientsClaim();
